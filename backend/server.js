f3a9e1d9f2be2aea5c8ba0fe42171b4cff039efd
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-09-16 11:34:10 +1000

    Config .env

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
 const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+const MODE = process.env.NODE_ENV;
+app.listen(PORT, console.log(`Server running in ${MODE} mode on port ${PORT}`));
