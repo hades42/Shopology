2cbe9d4f206d773a363a15a660604a53caa97fb7
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-09-17 21:11:12 +1000

    New features

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>COMP3120 Groups Assignment</h1>
+      <h1>COMP3120 Groups Assignment new feature</h1>
     </div>
   );
 }
