1f5c63b676d649d3c58e5ebca898d3ac7fe8c3d7
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-09-24 18:52:48 +1000

    Fixing navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import classes from "./App.module.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen/HomeScreen";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <Router>
+      <Navigation cartQty={"0"}></Navigation>
       <main className={classes.mainSection}>
         <Route path="/" component={HomeScreen} exact></Route>
       </main>
@@ -13,3 +15,4 @@ function App() {
 }
 
 export default App;
+
