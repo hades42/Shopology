1935e74954b376092dd23ca14393a757e669cd5a
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-09-25 13:07:45 +1000

    Add Best Seller section to homepage

--- client/src/screens/HomeScreen/HomeScreen.js
+++ client/src/screens/HomeScreen/HomeScreen.js
@@ -1,12 +1,14 @@
 import MainSection from "./MainSection";
 import TrendingSection from "./TrendingSection";
 import SaleSection from "./SaleSection";
+import BestSeller from "./BestSeller";
 const HomeScreen = () => {
   return (
     <div>
       <MainSection></MainSection>
       <TrendingSection></TrendingSection>
       <SaleSection></SaleSection>
+      <BestSeller></BestSeller>
     </div>
   );
 };
