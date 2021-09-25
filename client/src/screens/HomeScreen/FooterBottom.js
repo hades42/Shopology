2717fe42887dad1d4ae74760f7b674eaa46f7bce
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-09-25 17:05:17 +1000

    Create footer

---
+++ client/src/screens/HomeScreen/FooterBottom.js
@@ -0,0 +1,10 @@
+import classes from "./FooterBottom.module.css";
+const FooterBottom = () => {
+  return (
+    <div className={classes.container}>
+      <p>Copyright &copy; 2021 | All rights reserved</p>
+    </div>
+  );
+};
+
+export default FooterBottom;
