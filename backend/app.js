1da6bbb7baeb1ed0073eba230dbc4df9e4896182
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-10-05 00:52:40 +1100

    Install necessary packages

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const express = require("express");
 
+const connectDB = require("./config/db");
+connectDB();
+
 const app = express();
 
 app.get("/", (req, res) => {
