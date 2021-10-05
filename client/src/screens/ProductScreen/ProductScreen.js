70d2ca02f5025545547d06c23dfba497f74a8ab1
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-10-05 18:40:43 +1100

    Handle fetching product detail data

--- client/src/screens/ProductScreen/ProductScreen.js
+++ client/src/screens/ProductScreen/ProductScreen.js
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import classes from "./ProductScreen.module.css";
 import Container from "../../components/Container";
 import Subscribe from "../HomeScreen/Subscribe";
 import ProductSection from "./ProductSection/ProductSection";
 import ProductDescription from "./ProductDescription/ProductDescription";
 import ProductReview from "./ProductReview/ProductReview";
-const ProductScreen = () => {
+import { useDispatch, useSelector } from "react-redux";
+import { getProductDetail } from "../../actions/productActions";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+const ProductScreen = ({ match }) => {
+  const dispatch = useDispatch();
+  const productDetail = useSelector((state) => state.productDetail);
+  const { loading, error, product } = productDetail;
+
+  const productId = match.params.id;
+  useEffect(() => {
+    dispatch(getProductDetail(productId));
+  }, [dispatch, productId]);
+
   const [selection, setSelection] = useState("1");
 
   const changeSelection = (e) => {
@@ -20,26 +33,34 @@ const ProductScreen = () => {
   }
   return (
     <Container>
-      <ProductSection></ProductSection>
-      <div className={classes["selection-container"]}>
-        <ul className={classes.navSelection}>
-          <li
-            className={classes.navItem}
-            data-selection="1"
-            onClick={changeSelection}
-          >
-            Description
-          </li>
-          <li
-            className={classes.navItem}
-            data-selection="2"
-            onClick={changeSelection}
-          >
-            Reviews
-          </li>
-        </ul>
-        {selectionShow}
-      </div>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message>{error}</Message>
+      ) : (
+        <>
+          <ProductSection product={product}></ProductSection>
+          <div className={classes["selection-container"]}>
+            <ul className={classes.navSelection}>
+              <li
+                className={classes.navItem}
+                data-selection="1"
+                onClick={changeSelection}
+              >
+                Description
+              </li>
+              <li
+                className={classes.navItem}
+                data-selection="2"
+                onClick={changeSelection}
+              >
+                Reviews
+              </li>
+            </ul>
+            {selectionShow}
+          </div>
+        </>
+      )}
       <Subscribe></Subscribe>
     </Container>
   );
