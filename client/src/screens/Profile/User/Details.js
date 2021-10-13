fd46835eef4ea6667aba5ad1bfb316af4bd78f32
0MATRIX0 <bellamsujith4444666@gmail.com>
2021-10-14 03:41:48 +0530

    Changed Vans version to Details section of the user profilen

---
+++ client/src/screens/Profile/User/Details.js
@@ -0,0 +1,115 @@
+import { Form, Row, Col, Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../../../components/Message";
+import Loader from "../../../components/Loader";
+import { getUserDetails, updateUserProfile } from "../../../actions/userAction";
+import { useEffect, useState } from "react";
+import { USER_UPDATE_PROFILE_RESET } from "../../../constants/userConstants";
+
+const Details = ({ location, history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const userDetail = useSelector((state) => state.userDetail);
+  const { loading, error, user } = userDetail;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const updateLoading = userUpdateProfile.loading;
+  const updateError = userUpdateProfile.error;
+  const { success } = userUpdateProfile;
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push("/login");
+    } else {
+      if (!user || !user.name || success) {
+        // mainly reset success variable so we can update again
+        dispatch({ type: USER_UPDATE_PROFILE_RESET });
+        dispatch(getUserDetails("profile"));
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+      }
+    }
+  }, [history, userInfo, dispatch, user, success]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage("Password do not match");
+    } else {
+      dispatch(updateUserProfile({ id: user._id, name, email, password }));
+    }
+  };
+  console.log(success + " " + updateLoading);
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+        {error && <Message variant="danger">{error}</Message>}
+        {success && !updateLoading && (
+          <Message variant="success">Profile gets updated!</Message>
+        )}
+        {message && <Message variant="danger">{message}</Message>}
+        {loading && <Loader />}
+        {updateLoading && <Loader />}
+        {updateError && <Message variant="danger">{updateError}</Message>}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="confirmPassword">
+            <Form.Label>Confirm password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <button type="submit" variant="primary">
+            Update
+          </button>
+        </Form>
+      </Col>
+    </Row>
+  );
+};
+
+export default Details;
