d33db16455f91628addada2b43786a43882ca58e
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-10-14 19:52:17 +1100

    Creat dropdown notification

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,28 +1,24 @@
 import { Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
-function Modal() {
-  const [show, setShow] = useState(false);
+import { Modal } from "react-bootstrap";
+function DropNotif({ heading, text, resetData }) {
+  const [show, setShow] = useState(true);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleClose = () => {
+    setShow(false);
+    resetData();
+  };
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+        <Modal.Header>
+          <Modal.Title>{heading}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>{text}</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
@@ -30,4 +26,4 @@ function Modal() {
   );
 }
 
-export default Modal;
+export default DropNotif;
