fcae285bb1d66458942cc62cbe8c3225d5c652dd
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-10-14 19:54:21 +1100

    Update notification update for user profile

--- client/src/screens/Profile/User/UserProfile.js
+++ client/src/screens/Profile/User/UserProfile.js
@@ -1,24 +1,14 @@
 import React, { useState, useEffect } from "react";
-import {
-  ShopCategory,
-  ShopBanner,
-  ShopDesc,
-  ShopDescHome,
-} from "../../../components/Shop/Shop.elements";
-import {
-  LoginDescCategory,
-  Grids,
-  List,
-  ListItem,
-  LogoutButton,
-} from "./UserProfile.elements";
 import Details from "./Details";
 import Addresses from "./Addresses";
 import Orders from "./Orders";
 import { logout } from "../../../actions/userAction";
 import { useDispatch, useSelector } from "react-redux";
+import Request from "./Request";
+import { Container } from "react-bootstrap";
+import classes from "../../ProductScreen/ProductScreen.module.css";
 
-const UserProfile = ({ location, history }) => {
+const UserProfile = ({ history }) => {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
@@ -30,65 +20,65 @@ const UserProfile = ({ location, history }) => {
     }
   }, [history, userInfo]);
 
-  const [details, setDetails] = useState(true);
-  const [addresses, setAddresses] = useState(false);
-  const [orders, setOrders] = useState(false);
+  const [selection, setSelection] = useState("1");
 
-  const detailsHandler = () => {
-    setDetails(true);
-    setAddresses(false);
-    setOrders(false);
+  const changeSelection = (e) => {
+    setSelection(e.target.getAttribute("data-selection"));
   };
 
-  const addressesHandler = () => {
-    setDetails(false);
-    setAddresses(true);
-    setOrders(false);
-  };
-
-  const ordersHandler = () => {
-    setDetails(false);
-    setAddresses(false);
-    setOrders(true);
-  };
+  let selectionShow;
+  if (selection === "1") {
+    selectionShow = <Details></Details>;
+  } else if (selection === "2") {
+    selectionShow = <Addresses />;
+  } else if (selection === "3") {
+    selectionShow = <Orders />;
+  } else if (selection === "4") {
+    selectionShow = <Request />;
+  }
 
   const logoutHandler = (e) => {
+    e.preventDefault();
     dispatch(logout());
   };
 
   return (
-    <>
-      <ShopCategory>
-        <ShopBanner>My Account</ShopBanner>
-        <ShopDesc>
-          <ShopDescHome to="/">Home</ShopDescHome>
-          <LoginDescCategory />
-        </ShopDesc>
-      </ShopCategory>
-      <LogoutButton variant="text" onClick={logoutHandler}>
-        Log Out
-      </LogoutButton>
-      <Grids container spacing={2}>
-        <Grids item xs={4}>
-          <List>
-            <ListItem active={details} onClick={detailsHandler}>
-              My Details
-            </ListItem>
-            <ListItem active={addresses} onClick={addressesHandler}>
-              My Addresses
-            </ListItem>
-            <ListItem active={orders} onClick={ordersHandler}>
-              My Orders
-            </ListItem>
-          </List>
-        </Grids>
-        <Grids item xs={8}>
-          {details === true ? <Details /> : <></>}
-          {addresses === true ? <Addresses /> : <></>}
-          {orders === true ? <Orders /> : <></>}
-        </Grids>
-      </Grids>
-    </>
+    <Container>
+      <div className={classes["selection-container"]}>
+        <ul className={classes.navSelection}>
+          <li
+            className={classes.navItem}
+            data-selection="1"
+            onClick={changeSelection}
+          >
+            User Profile
+          </li>
+          <li
+            className={classes.navItem}
+            data-selection="2"
+            onClick={changeSelection}
+          >
+            User Addresses
+          </li>
+
+          <li
+            className={classes.navItem}
+            data-selection="3"
+            onClick={changeSelection}
+          >
+            User Order
+          </li>
+          <li
+            className={classes.navItem}
+            data-selection="4"
+            onClick={changeSelection}
+          >
+            User Request
+          </li>
+        </ul>
+        {selectionShow}
+      </div>
+    </Container>
   );
 };
 
