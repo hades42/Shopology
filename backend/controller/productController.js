4564a1a9201a155edfc2ba96a1996dc656c5cf28
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-10-24 12:39:02 +1100

    Update product endpoint

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -337,6 +337,32 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+const updatedProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
 module.exports = {
   getTrendingProducts,
   getTopProducts,
@@ -344,4 +370,5 @@ module.exports = {
   getProducts,
   createProductReview,
   deleteProduct,
+  updatedProduct,
 };
