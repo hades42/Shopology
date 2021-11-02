b9e117966ca129005542c3577173e37cc96ceace
0MATRIX0 <bellamsujith4444666@gmail.com>
2021-11-02 07:43:14 +0530

    added mobile variable to user profile

--- client/src/screens/Profile/User/UserProfile.js
+++ client/src/screens/Profile/User/UserProfile.js
@@ -11,6 +11,8 @@ import ProductListScreen from "../Admin/ProductList/ProductListScreen";
 import OrderListScreen from "../Admin/OrderList/OrderListScreen";
 
 const UserProfile = ({ history }) => {
+  let mobile = false
+
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
@@ -43,74 +45,87 @@ const UserProfile = ({ history }) => {
     selectionShow = <OrderListScreen />;
   }
 
+  if(window.screen.width < 1000) {
+    mobile = true
+  } else {
+    mobile = false
+  }
+
   return (
     <Container>
-      <div className={classes["selection-container"]}>
-        <ul className={classes.navSelection}>
-          <li
-            className={classes.navItem}
-            data-selection="1"
-            onClick={changeSelection}
-          >
-            User Profile
-          </li>
-          {userInfo && !userInfo.isAdmin && (
-            <li
-              className={classes.navItem}
-              data-selection="2"
-              onClick={changeSelection}
-            >
-              User Addresses
-            </li>
-          )}
-          {userInfo && !userInfo.isAdmin && (
-            <li
-              className={classes.navItem}
-              data-selection="3"
-              onClick={changeSelection}
-            >
-              User Order
-            </li>
-          )}
-          {userInfo && !userInfo.isAdmin && (
-            <li
-              className={classes.navItem}
-              data-selection="4"
-              onClick={changeSelection}
-            >
-              User Request
-            </li>
-          )}
-          {userInfo && userInfo.isAdmin && (
-            <li
-              className={classes.navItem}
-              data-selection="5"
-              onClick={changeSelection}
-            >
-              Manage Users
-            </li>
-          )}
-          {userInfo && userInfo.isAdmin && (
-            <li
-              className={classes.navItem}
-              data-selection="6"
-              onClick={changeSelection}
-            >
-              Manage Products
-            </li>
-          )}
-          {userInfo && userInfo.isAdmin && (
-            <li
-              className={classes.navItem}
-              data-selection="7"
-              onClick={changeSelection}
-            >
-              Manage Orders
-            </li>
-          )}
-        </ul>
-        {selectionShow}
-      </div>
+      {mobile
+        ?
+          <>LOL</>
+        :
+          <>
+            <div className={classes["selection-container"]}>
+              <ul className={classes.navSelection}>
+                <li
+                  className={classes.navItem}
+                  data-selection="1"
+                  onClick={changeSelection}
+                >
+                  User Profile
+                </li>
+                {userInfo && !userInfo.isAdmin && (
+                  <li
+                    className={classes.navItem}
+                    data-selection="2"
+                    onClick={changeSelection}
+                  >
+                    User Addresses
+                  </li>
+                )}
+                {userInfo && !userInfo.isAdmin && (
+                  <li
+                    className={classes.navItem}
+                    data-selection="3"
+                    onClick={changeSelection}
+                  >
+                    User Order
+                  </li>
+                )}
+                {userInfo && !userInfo.isAdmin && (
+                  <li
+                    className={classes.navItem}
+                    data-selection="4"
+                    onClick={changeSelection}
+                  >
+                    User Request
+                  </li>
+                )}
+                {userInfo && userInfo.isAdmin && (
+                  <li
+                    className={classes.navItem}
+                    data-selection="5"
+                    onClick={changeSelection}
+                  >
+                    Manage Users
+                  </li>
+                )}
+                {userInfo && userInfo.isAdmin && (
+                  <li
+                    className={classes.navItem}
+                    data-selection="6"
+                    onClick={changeSelection}
+                  >
+                    Manage Products
+                  </li>
+                )}
+                {userInfo && userInfo.isAdmin && (
+                  <li
+                    className={classes.navItem}
+                    data-selection="7"
+                    onClick={changeSelection}
+                  >
+                    Manage Orders
+                  </li>
+                )}
+              </ul>
+              {selectionShow}
+            </div>
+          </>
+      }
     </Container>
   );
 };
