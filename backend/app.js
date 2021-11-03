8f56b5c77c2126744c88f8140a362252a6b7cbba
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-11-03 15:53:53 +1100

    Fixing upload image error

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,13 @@ app.use("/api/user", userRoutes);
 app.use("/api/order", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
+if (process.env.NODE_ENV != "test") {
+  app.use("/backend/uploads", express.static(path.join(__dirname, "/uploads")));
+  app.get("/api/config/paypal", (req, res) =>
+    res.send(process.env.PAYPAL_CLIENT_ID)
+  );
+}
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(path.resolve("./"), "/client/build")));
 
@@ -41,13 +48,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-if (process.env.NODE_ENV != "test") {
-  app.get("/api/config/paypal", (req, res) =>
-    res.send(process.env.PAYPAL_CLIENT_ID)
-  );
-
-  app.use("/backend/uploads", express.static(path.join(__dirname, "/uploads")));
-}
 // Error Handler
 app.use(notFound);
 app.use(errorHandler);
