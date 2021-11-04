890231a959c0a3d192c2ed03de39887c5791011f
sepehrtorfehnezhad <s.torfeh.75@gmail.com>
2021-11-04 23:40:09 +1100

    adding the get all requests

--- backend/routes/requestRoutes.js
+++ backend/routes/requestRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const {
   createRequest,
+  getAllRequest,
 } = require("../controller/requestController");
 
 const { protect, adminProtect } = require("../middlewares/authMiddleware");
 
 router.route("/newSeller").post(protect, createRequest)
+router.route("/all").get(protect, adminProtect, getAllRequest)
 
 module.exports = router;
