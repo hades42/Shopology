70a634a0a7e84a54286d350c7c1b15d70b6d30af
sepehrtorfehnezhad <s.torfeh.75@gmail.com>
2021-11-04 23:36:55 +1100

    adding the get action

--- client/src/actions/requestAction.js
+++ client/src/actions/requestAction.js
@@ -3,6 +3,10 @@ import {
   REQUEST_SELLER_SUCCESS,
   REQUEST_SELLER_FAIL,
   REQUEST_SELLER_RESET,
+  GET_REQUEST_SELLER_REQUEST, 
+  GET_REQUEST_SELLER_SUCCESS, 
+  GET_REQUEST_SELLER_FAIL, 
+  GET_REQUEST_SELLER_RESET, 
 } from "../constants/requestConstant";
 
 import axios from "axios";
@@ -34,6 +38,41 @@ export const createRequest = (request) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: REQUEST_SELLER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+
+export const getAllRequest = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: GET_REQUEST_SELLER_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/request/all`, config);
+
+    dispatch({
+      type: GET_REQUEST_SELLER_SUCCESS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_REQUEST_SELLER_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
