27f3cf41a9dbd8fee3b062cfc2ad5f510f7958b4
sepehrtorfehnezhad <s.torfeh.75@gmail.com>
2021-11-04 20:32:04 +1100

    add request controller

---
+++ backend/controller/requestController.js
@@ -0,0 +1,30 @@
+const asyncHandler = require("express-async-handler");
+const Request = require("../model/requestModel");
+
+// @desc    Request for becoming a new seller
+// @route   POST /api/request/newSeller
+// @access  Public
+const createRequest = asyncHandler(async (req, res) => {
+  const { description } = req.body;
+  const user = req.user;
+
+  const newRequest = {
+      user: user._id,
+      description: description,
+  }
+
+  const result = await Request.create(newRequest);
+
+  if(result){
+      res.json(result)
+  } else {
+      res.status(404);
+      throw new Error("Couldn't create new request")
+  }
+
+});
+
+
+module.exports = {
+    createRequest,
+};
