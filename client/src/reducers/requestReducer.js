ac2e3910bdabdb76a7aef4db219093478ed30fb2
sepehrtorfehnezhad <s.torfeh.75@gmail.com>
2021-11-05 17:38:47 +1100

    adding new reducer for request

--- client/src/reducers/requestReducer.js
+++ client/src/reducers/requestReducer.js
@@ -6,7 +6,13 @@ import {
   GET_REQUEST_SELLER_REQUEST, 
   GET_REQUEST_SELLER_SUCCESS, 
   GET_REQUEST_SELLER_FAIL, 
-  GET_REQUEST_SELLER_RESET, 
+  GET_DESC_REQUEST_SELLER_REQUEST,
+  GET_DESC_REQUEST_SELLER_SUCCESS,
+  GET_DESC_REQUEST_SELLER_FAIL,
+  APPROVE_SELLER_REQUEST,
+  APPROVE_SELLER_SUCCESS,
+  APPROVE_SELLER_FAIL,
+  APPROVE_SELLER_RESET,
 } from "../constants/requestConstant";
 
 
@@ -25,7 +31,7 @@ export const requestSellerReducer = (state = { }, action) => {
   }
 };
 
-export const getRequestSellerReducer = (state = { request = [] }, action) => {
+export const getRequestSellerReducer = (state = { request: [] }, action) => {
     switch (action.type) {
         case GET_REQUEST_SELLER_REQUEST:
             return { loading: true , request: [] };
@@ -36,4 +42,32 @@ export const getRequestSellerReducer = (state = { request = [] }, action) => {
         default:
             return state;
     }
+}
+
+export const getRequestByIdReducer = (state = { request: {} }, action) => {
+  switch (action.type) {
+      case GET_DESC_REQUEST_SELLER_REQUEST:
+          return { loading: true , request: {} };
+      case GET_DESC_REQUEST_SELLER_SUCCESS:
+          return { loading: false, request: action.payload }
+      case GET_DESC_REQUEST_SELLER_FAIL:
+          return { loading: false, error: action.payload }
+      default:
+          return state;
+  }
+}
+
+export const approveRequestReducer = (state = { }, action) => {
+  switch (action.type) {
+      case APPROVE_SELLER_REQUEST:
+          return { loading: true };
+      case APPROVE_SELLER_SUCCESS:
+          return { loading: false, success: true }
+      case APPROVE_SELLER_FAIL:
+          return { loading: false, error: action.payload }
+      case APPROVE_SELLER_RESET:
+          return { }
+      default:
+          return state;
+  }
 }
