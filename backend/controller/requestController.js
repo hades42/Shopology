98f17dbc21ad691a05dbd4a8fb0759910584e5ef
sepehrtorfehnezhad <s.torfeh.75@gmail.com>
2021-11-05 14:20:54 +1100

    adding more checks for request backend

--- backend/controller/requestController.js
+++ backend/controller/requestController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Request = require("../model/requestModel");
+const User = require("../model/userModel");
 
 // @desc    Request for becoming a new seller
 // @route   POST /api/request/newSeller
@@ -8,19 +9,33 @@ const createRequest = asyncHandler(async (req, res) => {
   const { description } = req.body;
   const user = req.user;
 
-  const newRequest = {
-      user: user._id,
-      description: description,
-  }
+  const isRequestExists = await Request.find({user: user.id})
 
-  const result = await Request.create(newRequest);
+  console.log(isRequestExists)
 
-  if(result){
-      res.json(result)
+  if(isRequestExists){
+      res.status(400)
+      if(isRequestExists.approved){
+        throw new Error("Already request, and hase been approved")
+      } else {
+            throw new Error("Aleady requested, waiting for approval")
+      }
   } else {
-      res.status(404);
-      throw new Error("Couldn't create new request")
-  }
+
+        const newRequest = {
+            user: user._id,
+            description: description,
+        }
+    
+        const result = await Request.create(newRequest);
+    
+        if(result){
+            res.json(result)
+        } else {
+            res.status(404);
+            throw new Error("Couldn't create new request")
+        }
+    }
 
 });
 
@@ -45,7 +60,7 @@ const getAllRequest = asyncHandler(async (req, res) => {
 // @access  admin 
 const getRequestById = asyncHandler(async (req, res) => {
 
-    const re = await Request.find(req.param.id).populate("user", "id name")
+    const re = await Request.findById(req.params.id).populate("user", "id name")
 
     if(re){
         res.json(re)
@@ -56,8 +71,34 @@ const getRequestById = asyncHandler(async (req, res) => {
 })
 
 
+// @desc    Approve seller request
+// @route   PUT /api/request/approve/:id
+// @access  admin
+// @note    :id is user id, not a request id
+const approveUserRequest = asyncHandler(async (req, res) => {
+    
+    const user = await User.findById(req.params.id)
+
+    if(user){
+
+        user.isSeller = true
+        await user.save()
+        res.status(200)
+        res.json({
+            user: user._id,
+            isSeller: user.isSeller
+        })
+
+    } else {
+        res.status(404)
+        throw new Error("User not found")
+    }
+
+})
+
 module.exports = {
     createRequest,
     getAllRequest,
     getRequestById,
+    approveUserRequest,
 };
--- backend/routes/requestRoutes.js
+++ backend/routes/requestRoutes.js
@@ -4,6 +4,7 @@ const {
   createRequest,
   getAllRequest,
   getRequestById,
+  approveUserRequest,
 } = require("../controller/requestController");
 
 const { protect, adminProtect } = require("../middlewares/authMiddleware");
@@ -11,6 +12,7 @@ const { protect, adminProtect } = require("../middlewares/authMiddleware");
 router.route("/newSeller").post(protect, createRequest)
 router.route("/all").get(protect, adminProtect, getAllRequest)
 router.route("/:id").get(protect,adminProtect,getRequestById)
+router.route("/approve/:id").put(protect, adminProtect, approveUserRequest)
 
 
 module.exports = router;
