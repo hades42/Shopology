4826a3c46f58b4ef42a389d880b732ca19e5a5c7
sepehrtorfehnezhad <s.torfeh.75@gmail.com>
2021-11-05 17:36:14 +1100

    adding the new actions

--- client/src/actions/requestAction.js
+++ client/src/actions/requestAction.js
@@ -6,7 +6,14 @@ import {
   GET_REQUEST_SELLER_REQUEST, 
   GET_REQUEST_SELLER_SUCCESS, 
   GET_REQUEST_SELLER_FAIL, 
-  GET_REQUEST_SELLER_RESET, 
+  GET_REQUEST_SELLER_RESET,
+  GET_DESC_REQUEST_SELLER_REQUEST,
+  GET_DESC_REQUEST_SELLER_SUCCESS,
+  GET_DESC_REQUEST_SELLER_FAIL,
+  GET_DESC_REQUEST_SELLER_RESET,
+  APPROVE_SELLER_REQUEST,
+  APPROVE_SELLER_SUCCESS,
+  APPROVE_SELLER_FAIL
 } from "../constants/requestConstant";
 
 import axios from "axios";
@@ -80,3 +87,71 @@ export const getAllRequest = () => async (dispatch, getState) => {
     });
   }
 };
+
+export const approveRequest = (requestId) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: APPROVE_SELLER_REQUEST,
+      });
+  
+      const {
+        userLogin: { userInfo },
+      } = getState();
+  
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+  
+      const { data } = await axios.put(`/api/request/approve/${requestId}`, {} ,config);
+  
+      dispatch({
+        type: APPROVE_SELLER_SUCCESS,
+        payload: data
+      });
+    } catch (error) {
+      dispatch({
+        type: APPROVE_SELLER_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
+
+  export const getRequestById = (requestId) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: GET_DESC_REQUEST_SELLER_REQUEST,
+      });
+  
+      const {
+        userLogin: { userInfo },
+      } = getState();
+  
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+  
+      const { data } = await axios.get(`/api/request/${requestId}`, config);
+  
+      dispatch({
+        type: GET_DESC_REQUEST_SELLER_SUCCESS,
+        payload: data
+      });
+    } catch (error) {
+      dispatch({
+        type: GET_DESC_REQUEST_SELLER_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
